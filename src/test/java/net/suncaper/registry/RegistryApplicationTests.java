9294c273def9ab981b120474e33b75f25a563d58
Raoyou <jary0719@163.com>
2021-03-02 20:50:04 +0800

    项目上传

---
+++ src/test/java/net/suncaper/registry/RegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package net.suncaper.registry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegistryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
